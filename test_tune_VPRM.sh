1a9c4fe30bd9da19535e0816ae15bb5363e1a5d3
mareitzef <matze.reif@posteo.de>
2024-04-07 10:15:30 +0200

    for tests

--- test_tune_VPRM.sh
+++ test_tune_VPRM.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Define variables
-base_path="/home/madse/Downloads/Fluxnet_Data/Europe/"
+base_path="/scratch/c7071034/DATA/Fluxnet2015/Europe/"
 maxiter=1  # (default=100 takes ages)
 opt_method="diff_evo_V2"  # "minimize_V2","diff_evo_V2"
 VPRM_old_or_new="old"  # "old","new"
 
 # List of folders
 folders=(
-"FLX_DE-Hai_FLUXNET2015_FULLSET_2000-2012_1-4"
+"FLX_DE-Seh_FLUXNET2015_FULLSET_2007-2010_1-4"
 "FLX_RU-Fyo_FLUXNET2015_FULLSET_1998-2014_2-4"
 )
-folder="FLX_DE-Hai_FLUXNET2015_FULLSET_2000-2012_1-4"
+
 # Loop through each folder
 for folder in "${folders[@]}"; do
     # Call Python script with variables as command line arguments
