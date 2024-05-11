0068ad53df53f158146466685967edd00ab10cdc
mareitzef <matze.reif@posteo.de>
2024-05-11 19:19:33 +0200

    rename to V3

--- submit_jobs_tune_VPRM.sh
+++ submit_jobs_tune_VPRM.sh
@@ -3,7 +3,7 @@
 # Define variables
 base_path="/scratch/c7071034/DATA/Fluxnet2015/Alps/"
 maxiter=100  # (default=100 takes ages)
-opt_method="diff_evo_V2"  # "diff_evo_V2"
+opt_method="diff_evo_V3"  # "diff_evo_V2"
 VPRM_old_or_new="old"  # "old","new"
 
 # List of folders
