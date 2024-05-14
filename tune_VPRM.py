d3bf0b8a87c4d9814618bdb172a044a7555f8dcb
mareitzef <matze.reif@posteo.de>
2024-05-14 09:56:43 +0200

    added random uncertainty

--- tune_VPRM.py
+++ tune_VPRM.py
@@ -226,15 +226,17 @@ def main():
     ############################### Read FLUXNET Data ##############################################
 
     timestamp = "TIMESTAMP_START"
-    t_air = "TA_F"
-    gpp = "GPP_DT_VUT_REF"
+    t_air = "TA_F"  # Air temperature, consolidated from TA_F_MDS and TA_ERA
+    gpp = "GPP_DT_VUT_REF"  # Gross Primary Production, from Daytime partitioning method, reference selected from GPP versions using model efficiency (MEF).
     r_eco = "RECO_DT_VUT_REF"  # is just used for plotting and R2 out of interest
-    nee = "NEE_VUT_REF"
-    nee_qc = "NEE_VUT_REF_QC"
-    night = "NIGHT"
-    nee_mean = "NEE_VUT_REF_pos"
+    nee = "NEE_VUT_REF"  # Net Ecosystem Exchange, using Variable Ustar Threshold (VUT) for each year, reference selected on the basis of the model efficiency (MEF).
+    nee_qc = "NEE_VUT_REF_QC"  # Quality flag for NEE_VUT_REF
+    nee_randunc = "NEE_VUT_REF_RANDUNC"  # Random uncertainty for NEE_VUT_REF, from measured only data
+    nee_jointunc = "NEE_VUT_REF_JOINTUNC"  # Joint uncertainty estimation for NEE_VUT_REF, including random uncertainty and USTAR filtering uncertainty
+    night = "NIGHT"  # flag for nighttime
+    nee_mean = "NEE_VUT_REF_pos"  # only positive values from NEE_VUT_REF
     # TODO test: nee_cut = 'NEE_CUT_REF' and 'nee = 'NEE_VUT_REF''
-    sw_in = "SW_IN_F"
+    sw_in = "SW_IN_F"  # Shortwave radiation, incoming consolidated from SW_IN_F_MDS and SW_IN_ERA (negative values set to zero)
     columns_to_copy = [
         timestamp,
         night,
@@ -244,7 +246,9 @@ def main():
         nee,
         sw_in,
         nee_qc,
-        # nee_mean,  # TODO test the difference for NEE_VUT_MEAN
+        nee_randunc,
+        nee_jointunc,
+        # nee_mean,  # TODO quantify the difference for NEE_VUT_MEAN
     ]
     converters = {k: lambda x: float(x) for k in columns_to_copy}
     df_site = pd.read_csv(file_path, usecols=columns_to_copy, converters=converters)
@@ -791,7 +795,12 @@ def main():
                     "RMSE_NEE": [rmse_NEE],
                     "AIC": [AIC],
                     "NSE_NEE": [NSE_NEE],
+                    "RANDUNC": [df_year[nee_randunc].mean()],
+                    "JOINTUNC": [df_year[nee_jointunc].mean()],
+                    "RANDUNC_max": [df_year[nee_randunc].max()],
+                    "JOINTUNC_max": [df_year[nee_jointunc].max()],
                     "T_mean": [df_year[t_air].mean()],
+                    "T_max": [df_year[t_air].resample("D").max().mean()],
                     "lat": [latitude],
                     "lon": [longitude],
                     "elev": [elevation],
@@ -827,7 +836,12 @@ def main():
                     "RMSE_NEE": [rmse_NEE],
                     "AIC": [AIC],
                     "NSE_NEE": [NSE_NEE],
+                    "RANDUNC": [df_year[nee_randunc].mean()],
+                    "JOINTUNC": [df_year[nee_jointunc].mean()],
+                    "RANDUNC_max": [df_year[nee_randunc].max()],
+                    "JOINTUNC_max": [df_year[nee_jointunc].max()],
                     "T_mean": [df_year[t_air].mean()],
+                    "T_max": [df_year[t_air].resample("D").max().mean()],
                     "lat": [latitude],
                     "lon": [longitude],
                     "elev": [elevation],
