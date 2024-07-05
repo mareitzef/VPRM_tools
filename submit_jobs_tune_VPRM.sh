50c301df33d0e474458e20ff0860eb4a558791df
mareitzef <matze.reif@posteo.de>
2024-07-05 10:43:41 +0200

    improves submitting job by loops over domain and VPRM old/new directly

--- submit_jobs_tune_VPRM.sh
+++ submit_jobs_tune_VPRM.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
 # Define variables
-base_path="/scratch/c7071034/DATA/Fluxnet2015/Europe/"
-maxiter=100  # (default=100 takes ages)
-opt_method="diff_evo_V9"  # "diff_evo_V2"
-VPRM_old_or_new="new"  # "old","new"
+base_paths=(
+    "/scratch/c7071034/DATA/Fluxnet2015/Europe/"
+    "/scratch/c7071034/DATA/Fluxnet2015/Alps/"
+)
+maxiter=100  
+opt_method="diff_evo_V9"  # method an version
+VPRM_options=("new" "old") 
 
-# List of folders
-folders=($(find "$base_path" -type d -name "FLX_*"))
+# Loop through each base path
+for base_path in "${base_paths[@]}"; do
+    # Loop through each VPRM option
+    for VPRM_old_or_new in "${VPRM_options[@]}"; do
+        # List of folders
+        folders=($(find "$base_path" -type d -name "FLX_*"))
 
-# Loop through each folder
-for folder in "${folders[@]}"; do
-    # Extract folder name from path
-    folder_name=$(basename "$folder")
-    # Create SLURM script for each job
-    cat <<EOF >"job_${folder_name}_${VPRM_old_or_new}.sh"
+        # Loop through each folder
+        for folder in "${folders[@]}"; do
+            # Extract folder name from path
+            folder_name=$(basename "$folder")
+            # Create SLURM script for each job
+            cat <<EOF >"job_${folder_name}_${VPRM_old_or_new}.sh"
 #!/bin/bash
-#SBATCH --job-name=tune_VPRM_${folder_name}
+#SBATCH --job-name=tune_VPRM_${folder_name}_${VPRM_old_or_new}
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
@@ -26,6 +33,8 @@ module load python
 srun python tune_VPRM.py -p "$base_path" -f "$folder_name" -i "$maxiter" -m "$opt_method" -v "$VPRM_old_or_new"
 EOF
 
-    # Submit the job to the cluster
-    sbatch "job_${folder_name}_${VPRM_old_or_new}.sh"
+            # Submit the job to the cluster
+            sbatch "job_${folder_name}_${VPRM_old_or_new}.sh"
+        done
+    done
 done
